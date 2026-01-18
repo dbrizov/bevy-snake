e1a02982738425f7b514879ba6a67eaa24903300
dbrizov <d.b.rizov@gmail.com>
2026-01-18 16:12:03 +0200

    main.rs cleanup

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,10 +1,16 @@
 use bevy::prelude::*;
 
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(HelloPlugin)
+        .run();
+}
+
 pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
         app.add_systems(Startup, add_people);
         app.add_systems(Update, greet_people);
     }
@@ -16,28 +22,12 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(HelloPlugin)
-        .run();
-}
-
 fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+    commands.spawn((Person, Name("Denis Rizov".to_string())));
 }
 
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
-        }
+fn greet_people(time: Res<Time>, query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("{}, {:?}", time.delta_secs(), name.0);
     }
 }
