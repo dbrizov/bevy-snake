856ac1f0da01ae36b2f58bee687b2cd843fe8212
dbrizov <d.b.rizov@gmail.com>
2026-01-18 20:46:42 +0200

    Minimal main.rs

--- game/src/main.rs
+++ game/src/main.rs
@@ -12,31 +12,10 @@ fn main() {
         .insert_resource(FramepaceSettings {
             limiter: Limiter::from_framerate(FPS_TARGET),
         })
-        .add_plugins(HelloPlugin)
+        .add_systems(Update, print_fps)
         .run();
 }
 
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people);
-        app.add_systems(Update, greet_people);
-    }
-}
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Denis Rizov".to_string())));
-}
-
-fn greet_people(time: Res<Time>, query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("{}, {:?}", 1.0 / time.delta_secs(), name.0);
-    }
+fn print_fps(time: Res<Time>) {
+    println!("{}", 1.0 / time.delta_secs());
 }
